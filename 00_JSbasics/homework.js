7ad5580945ccedaa0992f851a32b62719a03c8c8
artemshakun <artem.shakun.91@gmail.com>
2022-03-16 11:32:10 +0200

    bug fix

--- 00_JSbasics/homework.js
+++ 00_JSbasics/homework.js
@@ -1,25 +1,26 @@
-// console.table(countries);
+console.table(countries);
 
 function getAveragePopulation(countries) {
-    var averagePopulation = countries.reduce((acc, currentValue) => acc + currentValue.population, 0) / (countries.length || 1);
+    if (!countries || !countries.length) {
+        return 0;
+    }
+    var averagePopulation = countries.reduce((acc, currentValue) => acc + currentValue.population, 0) / countries.length;
     return averagePopulation.toFixed(2);
 
         //в каждой стране есть поле population, вернуть из функции среднее арифм.
 };
 
 function getNames(countries) {
-    var arrayCountriesName = [];
-    countries.map(country => arrayCountriesName.push(country.name));
-    return arrayCountriesName;
+    var countriesNames = countries.map(country => country.name);
+    return countriesNames;
 
     //вернуть список (массив) имен стран (поле name)
 };
 
 function findCountry(countries) {
     var countryName = prompt('Enter country name:').toLowerCase();
-    var arrayCountriesName = getNames(countries);
-    return (arrayCountriesName.find(country => country.toLowerCase() === countryName)) ? true : false;
-    // return arrayCountriesName.includes(countryName); in this case we need to remove method toLowerCase() in variable countryName;
+    var countriesNames = getNames(countries);
+    return (countriesNames.some(country => country.toLowerCase() === countryName));
 
     //ввод через prompt название страны, вернуть true если такая страна нашлась по имени, false если не нашлась. можно использовать для этого getNames выше + indexOf
 };
@@ -27,14 +28,12 @@ function findCountry(countries) {
 function getCountryByCode(countries) {
     var countryCode = prompt('Enter country code: ').toUpperCase();
 
-    if (countryCode.length === 3) {
-        const isCountry = countries.filter(country => country.alpha3Code === countryCode);
-        if (isCountry.length === 0) {
-            return false
-        }
-        return isCountry;
+    if (countryCode.length !== 3) {
+        return 'Error! Your code invalid !!!';
     }
-    return 'Error! Your code invalid !!!';
+
+    const isCountry = countries.filter(country => country.alpha3Code === countryCode);
+    return (isCountry.length !== 0) ? isCountry : false;
 
     //пользователь вводит через prompt трехбуквенный код. если введенная строка не 3 символа длиной - выдать сообщение о неправильном вводе.
     //по введенной строке (трехбуквенный код) найти страну (должно совпадать с alpha3Code, которое есть у каждой страны).
@@ -46,4 +45,4 @@ function getCountryByCode(countries) {
 // console.log(getAveragePopulation(countries));
 // console.log(getNames(countries));
 // console.log(findCountry(countries));
-// console.log(getCountryByCode(countries))
+// console.log(getCountryByCode(countries));
