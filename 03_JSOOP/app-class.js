c4cecdec19d6ad4ff839cb33184e97dbe141ca4d
artemshakun <artem.shakun.91@gmail.com>
2022-03-31 15:28:13 +0300

    created classes

--- 03_JSOOP/app-class.js
+++ 03_JSOOP/app-class.js
@@ -6,7 +6,6 @@ class Auto {
         this.vin = vin;
     }
     log() {
-
         console.log(this.brand + ' ' + this.model + ' ' + this.year);
     }
     checkVin() {
@@ -17,8 +16,32 @@ class Auto {
     }
 }
 
-class 
-let newCar = new Auto('Audi', 'B6', '2020', 'AF12124AEGJ32413');
+class Auto_Fuel extends Auto {
+    constructor (brand, model, year, vin, engineСapacity, consumption) {
+        super(brand, model, year, vin);
+        this.engineСapacity = engineСapacity;
+        this.consumption = consumption;
+    }
+    showFuelConsumption() {
+        console.log(this.engineСapacity + ' ' + this.consumption);
+    }
+}
 
-newCar.log();
-console.log(newCar.checkVin());
+class Auto_Electric extends Auto {
+    constructor (brand, model, year, vin, batteryCapacity) {
+        super(brand, model, year, vin);
+        this.batteryCapacity = batteryCapacity;
+    }
+    showBatteryConfig() {
+        console.log(this.batteryCapacity)
+    }
+}
+
+let newCar = new Auto_Fuel('Audi', 'B6', '2020', 'AF12124AEGJ32413', '2л', '100км/ч');
+let tesla = new Auto_Electric('Tesla', 'Model X', 2020, 'AF12124AEGJ32413', '100 кВт/ч')
+
+// batteryCapacity
+tesla.log();
+console.log(tesla.checkVin());
+tesla.showBatteryConfig();
+newCar.showFuelConsumption()
